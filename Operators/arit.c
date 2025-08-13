45d0149570072e3eac23679e10d4c71dc10c0e3a
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-08-13 22:07:52 +0530

    .

--- Operators/arit.c
+++ Operators/arit.c
@@ -6,5 +6,12 @@ int main()
     int b = 2;
     int c = a + b;
     printf("The answer is : %d", c);  // <-- Added comma here
+
+    int d=4;
+    int e=5;
+    int f=e-d;
+    printf("The answer is &d",f);
     return 0;
+
 }
+
