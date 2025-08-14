bc0a8883c0d4ec9468996b01e019b0c435da1632
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2025-08-14 21:29:41 +0530

    .

--- Operators/compare.c
+++ Operators/compare.c
@@ -2,5 +2,18 @@
 
 int main()
 {
-   printf("This is comparesion operator");
+    int a = 5;
+    int b = 3;
+
+    int c = a != b;  // 1 (true)
+    int d = a == b;  // 0 (false)
+    int e = a > b;   // 1 (true)
+    int f = a < b;   // 0 (false)
+
+    printf("a != b : %d\n", c);
+    printf("a == b : %d\n", d);
+    printf("a > b  : %d\n", e);
+    printf("a < b  : %d\n", f);
+
+    return 0;
 }
